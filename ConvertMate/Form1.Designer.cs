6678bfeebd2209b72068ba61eb9c9733c3ebd9f8
Aleks <aleks@gmail.com>
2024-08-29 16:42:39 +0200

    Testing phase 1

--- ConvertMate/Form1.Designer.cs
+++ ConvertMate/Form1.Designer.cs
@@ -28,13 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.textBoxFrom = new System.Windows.Forms.TextBox();
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.listBoxCurrencies = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnMoveToFrom = new System.Windows.Forms.Button();
+            this.btnMoveToTo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Location = new System.Drawing.Point(454, 165);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTo.TabIndex = 1;
+            // 
+            // textBoxFrom
+            // 
+            this.textBoxFrom.Location = new System.Drawing.Point(454, 118);
+            this.textBoxFrom.Name = "textBoxFrom";
+            this.textBoxFrom.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFrom.TabIndex = 2;
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.Location = new System.Drawing.Point(635, 118);
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAmount.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(454, 268);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(281, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(454, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(454, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "To";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(635, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Amount";
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(454, 210);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(93, 13);
+            this.lbResult.TabIndex = 8;
+            this.lbResult.Text = "Result will be here";
+            // 
+            // listBoxCurrencies
+            // 
+            this.listBoxCurrencies.FormattingEnabled = true;
+            this.listBoxCurrencies.Location = new System.Drawing.Point(37, 51);
+            this.listBoxCurrencies.Name = "listBoxCurrencies";
+            this.listBoxCurrencies.Size = new System.Drawing.Size(184, 342);
+            this.listBoxCurrencies.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "All currencies";
+            // 
+            // btnMoveToFrom
+            // 
+            this.btnMoveToFrom.Location = new System.Drawing.Point(249, 162);
+            this.btnMoveToFrom.Name = "btnMoveToFrom";
+            this.btnMoveToFrom.Size = new System.Drawing.Size(108, 23);
+            this.btnMoveToFrom.TabIndex = 11;
+            this.btnMoveToFrom.Text = "Move to From field";
+            this.btnMoveToFrom.UseVisualStyleBackColor = true;
+            this.btnMoveToFrom.Click += new System.EventHandler(this.btnMoveToFrom_Click);
+            // 
+            // btnMoveToTo
+            // 
+            this.btnMoveToTo.Location = new System.Drawing.Point(249, 207);
+            this.btnMoveToTo.Name = "btnMoveToTo";
+            this.btnMoveToTo.Size = new System.Drawing.Size(108, 23);
+            this.btnMoveToTo.TabIndex = 12;
+            this.btnMoveToTo.Text = "Move to To field";
+            this.btnMoveToTo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(919, 512);
+            this.Controls.Add(this.btnMoveToTo);
+            this.Controls.Add(this.btnMoveToFrom);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBoxCurrencies);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.textBoxAmount);
+            this.Controls.Add(this.textBoxFrom);
+            this.Controls.Add(this.textBoxTo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBoxTo;
+        private System.Windows.Forms.TextBox textBoxFrom;
+        private System.Windows.Forms.TextBox textBoxAmount;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.ListBox listBoxCurrencies;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnMoveToFrom;
+        private System.Windows.Forms.Button btnMoveToTo;
     }
 }
 
--- ConvertMate/Form1.cs
+++ ConvertMate/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,120 @@ namespace ConvertMate
 {
     public partial class Form1 : Form
     {
+        private static readonly HttpClient client = new HttpClient();
         public Form1()
         {
             InitializeComponent();
+            LoadCurrencies();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        public async void calculate()
+        {
+            try
+            {
+                string apiKey = "f42f1685feea33e8ed4d0b9d";
+                string baseCurrency = textBoxFrom.Text.ToUpper();
+                string targetCurrency = textBoxTo.Text.ToUpper();
+                string amountText = textBoxAmount.Text;
+
+                if (string.IsNullOrEmpty(baseCurrency) || string.IsNullOrEmpty(targetCurrency) || string.IsNullOrEmpty(amountText))
+                {
+                    MessageBox.Show("Please fill in all fields", "Input Error");
+                    return;
+                }
+
+                // Construct URL for the API request
+                string url = $"https://v6.exchangerate-api.com/v6/{apiKey}/latest/{baseCurrency}";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        JObject json = JObject.Parse(responseBody);
+
+                        if (json["conversion_rates"]?[targetCurrency] != null)
+                        {
+                            decimal conversionRate = json["conversion_rates"][targetCurrency].Value<decimal>();
+
+                            if (decimal.TryParse(amountText, out decimal amount))
+                            {
+                                decimal convertedAmount = amount * conversionRate;
+                                lbResult.Text = $"{amount} {baseCurrency} is equal to {convertedAmount:F2} {targetCurrency}";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Please enter a valid number", "Input Error");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Conversion rate for {targetCurrency} not found", "Error");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to retrieve data", "Error");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error");
+            }
+        }
+
+        private async void LoadCurrencies()
+        {
+            try
+            {
+                string apiKey = "f42f1685feea33e8ed4d0b9d";
+                string url = $"https://v6.exchangerate-api.com/v6/{apiKey}/latest/USD";
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    JObject json = JObject.Parse(responseBody);
+
+                    var currencies = json["conversion_rates"].ToObject<Dictionary<string, object>>().Keys;
+                    var sortedCurrencies = new SortedSet<string>(currencies);
+
+                    listBoxCurrencies.Items.Clear();
+                    foreach (var currency in sortedCurrencies)
+                    {
+                        listBoxCurrencies.Items.Add(currency);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Failed to retrieve currency data", "Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error");
+            }
+        }
+
+        private void btnMoveToFrom_Click(object sender, EventArgs e)
+        {
+            if (listBoxCurrencies.SelectedIndex != -1)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Please select a currency");
+            }
         }
     }
 }
